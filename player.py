a0840e4964a60557b093199d09a1c11ec5c80f66
Brick <brick.berserker@gmail.com>
2024-12-28 11:34:21 +0000

    PEW PEW PEW...

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from shot import Shot
 import pygame
 
 
@@ -33,7 +34,15 @@ class Player(CircleShape):
 			self.move(-dt)
 		if keys[pygame.K_w]:
 			self.move(dt)
+		if keys[pygame.K_SPACE]:
+			self.shoot()
 
 	def move(self, dt):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
 		self.position += forward * PLAYER_SPEED * dt
+
+	def shoot(self):
+		shot_velocity = pygame.Vector2(0, 1) * PLAYER_SHOOT_SPEED
+		shot_velocity = shot_velocity.rotate(self.rotation)
+		shot = Shot(self.position.x, self.position.y)
+		shot.velocity = shot_velocity
