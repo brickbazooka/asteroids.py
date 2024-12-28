91750880128d26f935fa7d4528c0848b6a6f2fd8
Brick <brick.berserker@gmail.com>
2024-12-28 10:45:20 +0000

    Add player functionality

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from constants import *
 import pygame
+from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	clock = pygame.time.Clock()
+	dt = 0
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+	
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill("#000000")
+
+		screen.fill("black")
+
+		for sprite in updatable:
+			sprite.update(dt)
+
+		for sprite in drawable:
+			sprite.draw(screen)
+
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
